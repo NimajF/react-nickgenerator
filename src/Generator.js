a45f49f65e38ce89661c856d1517b450b924a0b9
NimajF <benjafran2000@gmail.com>
2022-02-11 00:44:25 +0100

    Add reset button

--- src/Generator.js
+++ src/Generator.js
@@ -4,8 +4,8 @@ import { capitalize } from "./utils/Strings";
 import { nickContext } from "./contexts/nickContext";
 import "./Generator.css";
 
-export function Generator(){
-    const { setNickname } = useContext(nickContext);
+export function Generator(props){
+    const { setNickname, setClick } = useContext(nickContext);
     const generateNick = async () => {
         const fetchData = async (endpoint) => {
             return capitalize((await axios.get(`https://random-word-form.herokuapp.com/random/${endpoint}`)).data[0].replaceAll("-", " ")).replaceAll(" ", "");
@@ -13,13 +13,18 @@ export function Generator(){
         let randomAdjective = await fetchData('adjective');
         let subject = await (Math.floor(Math.random() * 2) ? fetchData('noun') : fetchData('animal'));
         let randomNumber = Math.floor(Math.random() * 999);
-        setNickname(`${randomAdjective}${subject}${randomNumber}`);
+        props.open ? setNickname(`${props.provideText}${subject}${randomNumber}`) : setNickname(`${randomAdjective}${subject}${randomNumber}`);
+    }
+
+    const reset = () => {
+        setNickname("");
+        setClick(false);
     }
 
     return (
         <div className="div-btns" >
             <button className="generate" onClick={generateNick} >Generate</button>
-            <button className="reset" onClick={() => setNickname("") }>Reset</button>
+            <button className="reset" onClick={reset}>Reset</button>
         </div>
     )
 }
