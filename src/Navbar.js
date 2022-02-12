1a88eaeb5a84bbf9e8745dd28374cec1ca3296c6
NimajF <benjafran2000@gmail.com>
2022-02-12 19:23:26 +0100

    Tweak styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,20 @@
 import "./Navbar.css";
 
 export function Navbar() {
-    return <div className="navbar" >
-        <h1>React NickGenerator</h1>
-        <div>
-            <label className="switch">
-                <input type="checkbox"/>
-                <span className="slider"/>
-            </label>
+    return (
+        <div className="navbar" >
+            <div className="nav-logo">
+                <h1 className="nav-title" >NickGenerator</h1>
+                <span className="dice-emoji" >🎲</span>
+            </div>
+            <div>
+                {/* <label className="switch">
+                    <input type="checkbox"/>
+                    <span className="slider"/>
+                </label> */}
+            </div>
+            
+            
         </div>
-        
-        
-    </div>
+    )
 }
